cf60401e7525895c629813fd3af7d0e03bbab9ac
Tutz26 <tutz_26@hotmail.com>
2020-03-09 08:59:50 -0700

    First commit created on branch dev

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    private CharacterController characterController;
+
+        float vMovement = 0f;
+        float hMovement = 0f;
+        
+        Vector3 movement = Vector3.zero;
+
+        public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (true/*characterController.isGrounded*/){
+
+            vMovement = Input.GetAxis("Vertical");
+            hMovement = Input.GetAxis("Horizontal");
+
+            movement = new Vector3(hMovement * speed, 0f, vMovement * speed);
+
+        }
+
+        characterController.Move(movement * Time.deltaTime);
+
+    }
+}
